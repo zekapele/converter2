bf4c4415caa0dd2a0005477343b784cc7708eefd
jekapele <zekapele@gmail.com>
2025-03-08 16:54:49 +0200

    Додано функцію конвертації Фаренгейт → Цельсій

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+public class Main {  // Оголошуємо клас Main
+    // Функція для конвертації з Фаренгейтів в Цельсії
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("information");
+        // Викликаємо функцію та виводимо результат
+        System.out.println("100°F = " + fahrenheitToCelsius(100) + "°C");
     }
 }
